d970196460a0d45f1b3e27bf87c20217fea5efd0
namlato102 <namdang2798@gmail.com>
2024-09-19 01:00:10 +0200

    disable spring security

--- src/main/java/com/namlato/laptopshop/LaptopShopApplication.java
+++ src/main/java/com/namlato/laptopshop/LaptopShopApplication.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 @RestController
 public class LaptopShopApplication {
 
