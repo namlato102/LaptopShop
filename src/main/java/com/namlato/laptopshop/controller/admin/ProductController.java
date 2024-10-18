8b65648e2728a3c6c4eecd2c985fb47266f9f203
namlato102 <namdang2798@gmail.com>
2024-10-18 11:09:37 +0200

    [added] products layout and product create layout for admin

--- src/main/java/com/namlato/laptopshop/controller/admin/ProductController.java
+++ src/main/java/com/namlato/laptopshop/controller/admin/ProductController.java
@@ -2,13 +2,20 @@ package com.namlato.laptopshop.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import com.namlato.laptopshop.domain.Product;
 
 @Controller
 public class ProductController {
 
     @GetMapping("/admin/product")
-    public String getDashboard() {
-        return "admin/product/show";
+    public String getProduct() {
+        return "admin/product/products";
+    }
+
+    @GetMapping("/admin/product/create")
+    public String getCreateProductPage(Model model) {
+        model.addAttribute("newProduct", new Product());
+        return "admin/product/create";
     }
 }
-
