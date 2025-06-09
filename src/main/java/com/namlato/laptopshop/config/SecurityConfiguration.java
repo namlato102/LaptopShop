59eb94bbef765e145fc206bc425a2fe5f197ccb2
namlato102 <namdang2798@gmail.com>
2025-06-09 22:36:21 +0200

    [hotfix] redirect user to register page when account not exist

--- src/main/java/com/namlato/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/com/namlato/laptopshop/config/SecurityConfiguration.java
@@ -64,7 +64,8 @@ public class SecurityConfiguration {
                         .dispatcherTypeMatchers(DispatcherType.FORWARD,
                                 DispatcherType.INCLUDE)
                         .permitAll()
-                        .requestMatchers("/", "/login", "/product/**", "/client/**", "/css/**", "/js/**", "/images/**")
+                        .requestMatchers("/", "/login", "/product/**", "/register",
+                                "/client/**", "/css/**", "/js/**", "/images/**")
                         .permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
